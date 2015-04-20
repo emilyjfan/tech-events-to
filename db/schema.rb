557cd147805267c0207c84f134f1956fb06bfe50
emilyjfan <emilyjustinefan@gmail.com>
2015-04-19 20:48:11 -0400

    Add group_id to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419231607) do
+ActiveRecord::Schema.define(version: 20150420004403) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150419231607) do
     t.string   "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "group_id"
   end
 
   create_table "groups", force: :cascade do |t|
