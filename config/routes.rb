1158844e9e44e8f183d0445cc86fccbf754a7c26
emilyjfan <emilyjustinefan@gmail.com>
2015-04-19 20:48:52 -0400

    Add routes for groups and events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+resources :groups, only: [:index, :show]
+resources :events, only: [:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
