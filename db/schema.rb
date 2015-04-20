3b76fed45e749a1f2a4c008ae7e6bcd73db5922f
emilyjfan <emilyjustinefan@gmail.com>
2015-04-19 22:03:43 -0400

    Add image url to group model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420004403) do
+ActiveRecord::Schema.define(version: 20150420015331) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150420004403) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_url"
   end
 
 end
