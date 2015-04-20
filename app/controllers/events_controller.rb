ca0047b258fb29c41450ab272d288e9aa42513a9
emilyjfan <emilyjustinefan@gmail.com>
2015-04-19 21:52:00 -0400

    Update event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,11 @@
 class EventsController < ApplicationController
+
+	def index
+		@events = Event.all.includes(:group).order(:date)
+	end
+
+	def show
+		@event = Event.find(params[:id])
+	end
+
 end
