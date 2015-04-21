d0dcf73893438fff000616095e3d5a34fad04261
emilyjfan <emilyjustinefan@gmail.com>
2015-04-20 23:34:50 -0400

    Update Event class
    - Create client instance method
    - defined self.all method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event
+	
+  @client = RMeetup::Client.new do |config|
+    config.api_key = Figaro.env.meetup_api_key
+  end
+
+	def self.all(options = {})
+		results = @client.fetch(:open_events, { city: 'Toronto', country: 'CA' })
+	  
+	  results.map do |result|
+	  	new
+	  end
+	end
 
 
 end
