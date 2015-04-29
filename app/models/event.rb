2ccfd23d6a5c3f647c236053b9fd8748d6f56653
emilyjfan <emilyjustinefan@gmail.com>
2015-04-29 16:32:22 -0400

    Remove attr_reader :name method

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event
 
-	attr_reader :event, :name
+	attr_reader :event
 
 	def self.all(options = {})
 		response = @client.fetch(:open_events, {:city => "Toronto", :country => "CA", :topic => "softwaredev" })
@@ -9,7 +9,7 @@ class Event
 	  	r.event
 	  end
 	end
-  
+
   @client = RMeetup::Client.new do |config|
     config.api_key = Figaro.env.meetup_api_key
   end
