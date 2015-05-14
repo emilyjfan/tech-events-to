7e2c452950554a7221a377ceba324f8650786c11
emilyjfan <emilyjustinefan@gmail.com>
2015-05-14 17:05:01 -0400

    Update map
    - change zoom to 12
    - add eventName param to addMarker function
    - update contentString for infowindow dynamically

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -6,29 +6,28 @@ function Map(mapId){
 Map.prototype.init = function(latitude, longitude) {
 	var options = {
 		center: {lat: latitude, lng: longitude},
-		zoom: 14,
+		zoom: 12,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
 	this.canvas = new google.maps.Map(this.mapId, options);
 }
 
-
-
-Map.prototype.addMarker = function(latitude, longitude) {
+Map.prototype.addMarker = function(latitude, longitude, eventName) {
 	var options = {
 		position: new google.maps.LatLng(latitude, longitude),
 		map: this.canvas 
 	}
+
 	var myMarker = new google.maps.Marker(options);
-	this.markers.push(myMarker);
-	
-	var contentString = "Test text"
+		this.markers.push(myMarker);
 
-	var infowindow = new google.maps.InfoWindow({
-		content: contentString
-	});
+	var contentString = eventName;
+  
+  var infowindow = new google.maps.InfoWindow({
+   content: contentString
+  });
 
-	var that = this;
+  var that = this;
 
 	google.maps.event.addListener(myMarker, 'click', function() {
 	  infowindow.open(that.canvas,myMarker);
@@ -36,21 +35,19 @@ Map.prototype.addMarker = function(latitude, longitude) {
 }
 
 
+
 $(document).on('page:load ready', function(){
 	if ($('#map-canvas').length) {
 
 		var myMap = new Map($('#map-canvas')[0]);
-		myMap.init(43.6426, -79.3871);
-		// myMap.addMarker(latitude, longitude)
+		myMap.init(43.668861, -79.398096);
 
 		if (window.events) {
       events.forEach(function(coord) {
       	if (coord != null) {
-  	      myMap.addMarker(parseFloat(coord.latitude), parseFloat(coord.longitude));
+  	      myMap.addMarker(parseFloat(coord.latitude), parseFloat(coord.longitude), coord.eventname);
       	} 
       });
     }
-
-
 	}
 });
