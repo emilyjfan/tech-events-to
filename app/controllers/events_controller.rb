162025272d26d1b19f2951cd5881cadeecc90f9d
emilyjfan <emilyjustinefan@gmail.com>
2016-02-15 21:51:13 -0500

    refacting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,11 +2,13 @@ class EventsController < ApplicationController
 
 	def index
 		@events = Event.all
-    @date = Date.parse(params[:event][:date])
-  end
 
-  def show
-    @event = Event.find(params[:id])
+    @date = Date.parse(params[:event][:date]) if params[:event]
+
+    @events_for_date =  @events.map do |event|
+
+                        @date == Time.at(event['time']/1000).to_date
+                        end
   end
 
 end
