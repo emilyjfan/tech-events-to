585d2a1c0bb7623724703ebd7a89ace9b6ac0af2
emilyjfan <emilyjustinefan@gmail.com>
2016-03-05 21:20:49 -0500

    refactoring events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,18 +6,29 @@ class EventsController < ApplicationController
     @date = Date.today
     @date = Date.parse(params[:event][:date]) if params[:event]
 
-    @events_for_date = @events.find_all do |event|
+    @events_for_date = find_events_for_date
+    @events_with_venues = find_events_with_venues
+    @hash = build_markers
+  end
+
+  private
+
+  def find_events_with_venues
+    @events_for_date.delete_if { |k,v| k['venue'] == nil }
+  end
+
+  def find_events_for_date
+    @events.find_all do |event|
       Time.at(event['time']/1000).to_date == @date
     end
+  end
 
-    events_with_venues = @events_for_date.delete_if { |k,v| k['venue'] == nil }
-
-    @hash = Gmaps4rails.build_markers(events_with_venues) do |event, marker|
-      if event['venue']
-        marker.lat event['venue']['lat']
-        marker.lng event['venue']['lon']
-        marker.infowindow event['name']
-      end
+  def build_markers
+    Gmaps4rails.build_markers(@events_with_venues) do |event, marker|
+      marker.lat event['venue']['lat']
+      marker.lng event['venue']['lon']
+      marker.infowindow event['name']
     end
   end
+
 end
